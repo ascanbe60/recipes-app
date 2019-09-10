17f7885be4ba8145d56ad28ba9d02eb249f0c7c7
Guy Hunter-Rodwell <guy.hunter-rodwell@immediate.co.uk>
2019-09-10 09:58:31 +0100

    Fixing prisma config and input styling

--- frontend/styles/Form.js
+++ frontend/styles/Form.js
@@ -7,11 +7,10 @@ const Form = styled.form`
     margin-bottom: 1rem;
     input {
       padding: .5rem;
+      border-radius: 5px;
+      border: solid 1px rgba(0,0,0,.2);
+      height: 22px;
     }
-  }
-  input {
-    border-radius: 5px;
-    border: solid 1px rgba(0,0,0,.2);
   }
   @media (min-width: 768px) {
     max-width: 500px;
